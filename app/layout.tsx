27c6404178b6f87faf2ad61267a4228e9323d601
Saeed Alzaabi <almajed2007@hotmail.com>
2025-08-04 02:32:17 +0400

    Updated homepage content

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,24 @@ export default function RootLayout({
             gtag('config', 'G-ZFGHTCDZQ3');
           `}
         </Script>
+
+        {/* Twitter Pixel */}
+        <Script id="twitter-pixel" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','q9fit');
+          `}
+        </Script>
+
+        {/* Twitter conversion tracking event code */}
+        <Script id="twitter-conversion-tracking" strategy="afterInteractive">
+          {`
+            // Insert Twitter Event ID
+            twq('event', 'tw-q9fit-q9t9x', {});
+          `}
+        </Script>
       </head>
       <body>
         <WalletContextProvider>{children}</WalletContextProvider>
